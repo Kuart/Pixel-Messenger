516b63939c06382794434ea24b88c089209880bd
Denis Osmakov <Kuart44@gmail.com>
2021-06-10 20:06:11 +0300

    pixel parser methods

--- src/utils/pixel/pixelParser.ts
+++ src/utils/pixel/pixelParser.ts
@@ -1,9 +1,36 @@
+interface PixelElement {
+  [key: string]: any;
+}
+interface PixelNode {
+  [key: string]: any;
+}
+
 class PixelParser {
   private template: string;
 
   constructor(template: string) {
     this.template = template;
-    this.parseHTMLtoAST();
+  }
+
+  parseHTML() {
+    console.log(this.template);
+  }
+
+  createElement(tag: string, props: Record<string, string | number | boolean>, children: PixelElement[]): PixelNode {
+    const childrenElements = children.map((item) => this.createElement(item.tag, item.props, item.children));
+    return {
+      tag,
+      props,
+      childrenElements,
+    };
+  }
+
+  /* !replace any */
+  render(element: any, parent: any): void {
+    /* global window */
+    const domEl = window.document.createElement(element);
+    parent.appEndChild(domEl);
+    console.log(this.template);
   }
 }
 
